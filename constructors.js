5d4891eb5e500eb17d58634eb806e9fe62147c91
Mark McIver <mark@Marks-MacBook-Pro.local>
2019-02-18 16:56:46 -0700

    moved content for js5

--- constructors.js
+++ constructors.js
@@ -20,7 +20,7 @@
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
-function Employee(name, email, hireDate) {
+function Employee(name, email, hireDate, salary) {
   this.name = name;
   this.email = email;
   this.hireDate = hireDate;
@@ -36,7 +36,7 @@ function Employee(name, email, hireDate) {
 
 
 
-////////// PROBLEM 4 //////////
+////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
 var prius = new Car('Toyota', 'Prius', 2011);
@@ -54,5 +54,60 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
 */
 
 // Code here
+
+
+////////// PROBLEM 4 ////////// 	
+
+
+ // Here we have a constructor function named Movie that takes in 3 parameters: name (a string), genre (a string), and rating (a number withing 1-100 that has been averaged out from previous given ratings). Write a prototype method for the Movie constructor function called changeRating. This method should take in a number as a parameter that will be a new rating. Find the average between the old rating and the new rating. Change the rating property to become this new number and return the updated rating. 	
+
+
+ function Movie(name, genre, rating) {	
+  this.name = name;	
+  this.genre = genre;	
+  this.rating = rating;	
+}	
+
+
+ // Code here	
+
+
+
+
+////////// PROBLEM 5 //////////	
+
+
+ // Write a constructor function called User. This function should take in 4 parameters called name, age, email, and savedPosts in that order. Name and email will be strings, age will be a number and savedPosts will be an array of objects. These objects will each have 3 properties: id (a number), title (a string), and rating (a number between 1 and 5). These objects are the posts that the user will have saved to their account. 	
+
+ // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).	
+
+
+ // Code here	
+
+
+
+
+
+////////// PROBLEM 6 //////////
+
+
+ // You will be using the constructor function you just created in problem 5. 	
+// Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array. 	
+
+
+ // Code here	
+
+
+
+
+
+////////// PROBLEM 7 //////////
+
+
+ // You will continue to use the constructor function you created in problem 5.	
+// Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.	
+
+
+ // Code here
 
 
